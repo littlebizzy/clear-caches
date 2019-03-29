922952b3c69ae2f35167923402da8c83b38cb307
Jesse Nickles <jesse@littlebizzy.com>
2019-03-29 17:34:09 +0700

    Tweak clear PHP OPcache wording

--- admin/views/opcache.php
+++ admin/views/opcache.php
@@ -25,7 +25,7 @@ class OpCache extends Libraries\View_Display {
 			<td class="section"><label><h4>PHP OPcache</h4></label></td>
 			<?php if (!$loaded) : ?><td>The PHP OPcache extension is not installed.</td>
 			<?php elseif (!$enabled) : ?><td>The PHP OPcache extension is not enabled.</td>
-			<?php else : ?><td id="clrchs-action-opcache" class="clrchs-action"><input type="button" class="button button-primary clrchs-purge-button clrchs-purge-request" value="Purge Now!" /></td><?php endif; ?>
+			<?php else : ?><td id="clrchs-action-opcache" class="clrchs-action"><input type="button" class="button button-primary clrchs-purge-button clrchs-purge-request" value="Clear PHP OPcache" /></td><?php endif; ?>
 		</tr>
 
 	<?php }
