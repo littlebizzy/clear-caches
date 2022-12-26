55d1b31ce95a187322f4b25b4acf03b1b2454257
Jesse Nickles <jesse@littlebizzy.com>
2022-12-26 23:54:11 +0700

    Create clear-caches.php

---
+++ clear-caches.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Clear Caches
+Plugin URI: https://www.littlebizzy.com/plugins/clear-caches
+Description: Purge all of the WordPress caches
+Version: 1.2.3
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+PBP Version: 1.2.0
+WC requires at least: 3.3
+WC tested up to: 3.5
+Prefix: CLRCHS
+*/
